951e765e3a530919b00cf1b108c3508bb6257a21
irfanonk <onk.irfan@gmail.com>
2024-03-31 02:58:59 +0300

    add products route

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 mod routes;
+use dotenv::dotenv;
+use std::env;
+use std::sync::Arc;
+use tokio_postgres::{Client, Error, NoTls};
+
+async fn connect() -> Result<Arc<Client>, Error> {
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+
+    let (client, connection) = tokio_postgres::connect(&db_url, NoTls).await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    Ok(Arc::new(client))
+}
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let client = match connect().await {
+        Ok(client) => client,
+        Err(e) => {
+            eprintln!("connection error: {}", e);
+            return;
+        }
+    };
     // Import the routes from routes.rs
-    let routes = routes::create_routes();
+    let routes = routes::create_routes(client);
 
     // Start the Warp server
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio_postgres::{Client, Error};
 use warp::{Filter, Rejection, Reply};
-
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct User {
     pub id: u64,
@@ -10,8 +11,65 @@ pub struct User {
     pub password: Option<String>,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Product {
+    pub name: String,
+    pub manufacturer: String,
+    pub description: String,
+    pub added: String,
+    pub item_type: String,
+    pub tags: String,
+    pub slug: String,
+}
+
+// Function to read products from the database
+async fn read_products(client: &Client) -> Result<Vec<Product>, Error> {
+    let rows = client.query("SELECT * FROM product", &[]).await?;
+
+    let mut products = Vec::new();
+
+    for row in rows {
+        let name: String = row.get("name");
+        let description: String = row.get("description");
+        let manufacturer: String = row.get("manufacturer");
+        let added: String = row.get("added");
+        let item_type: String = row.get("itemType");
+        let tags: String = row.get("tags");
+        let slug: String = row.get("slug");
+
+        let product = Product {
+            name,
+            description,
+            manufacturer,
+            added,
+            item_type,
+            tags,
+            slug,
+        };
+        products.push(product);
+    }
+
+    Ok(products)
+}
+
+pub async fn get_products_handler(client: Arc<Client>) -> Result<impl Reply, Rejection> {
+    let products = read_products(&client).await.map_err(|e| {
+        eprintln!("Database error: {}", e);
+        warp::reject::reject()
+    })?;
+
+    Ok(warp::reply::json(&products))
+}
+// Helper function to inject the database client into warp filter chain
+fn with_db(
+    client: Arc<Client>,
+) -> impl Filter<Extract = (Arc<Client>,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || client.clone())
+}
 // Define a function to create the routes
-pub fn create_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+pub fn create_routes(
+    client: Arc<Client>,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     // Define a route for GET requests to "/"
     let hello_route = warp::path::end().map(|| warp::reply::html("Hello, world!"));
 
@@ -29,9 +87,13 @@ pub fn create_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> +
         // Serialize the User object to JSON and return it
         warp::reply::json(&user)
     });
+    let get_products_route = warp::path("products")
+        .and(warp::get())
+        .and(with_db(client.clone()))
+        .and_then(get_products_handler);
 
     // Combine the routes
-    let routes = hello_route.or(get_user_route);
+    let routes = hello_route.or(get_user_route).or(get_products_route);
 
     routes
 }
